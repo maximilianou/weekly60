d0a5a12b26193c5a466722eeb1ff7cd5707301f5
maximilianou <maximilianou@gmail.com>
2022-08-04 21:54:23 +0200

    feat(eth): add solidity and openzeppelin

---
+++ blog5/contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,12 @@
+// contracts/governance_standard/TimeLock.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+contract TimeLock is TimelockController {
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
+  ) TimelockController(minDelay, proposers, executors) {
+  }
+}
